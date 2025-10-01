62ed43a3216885530f2c38005d226fadf1611c8e
Maksym Vasylyshyn <maxim24152771@gmail.com>
2025-10-01 16:27:55 +0300

    add category dialog

---
+++ src/WebUI/src/app/modules/expense/components/add-category-dialog/add-category-dialog.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-category-dialog',
+  templateUrl: './add-category-dialog.html',
+  styleUrl: './add-category-dialog.scss'
+})
+export class AddCategoryDialog {
+
+}
