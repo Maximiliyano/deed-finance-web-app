4554e47842f9f8a4982176d11e56bf9cbe601bbe
Maksym Vasylyshyn <maxim24152771@gmail.com>
2025-10-13 10:53:10 +0300

    angular update 19v

--- src/WebUI/src/app/shared/services/category.service.ts
+++ src/WebUI/src/app/shared/services/category.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {environment} from "../../../environments/environment.development";
-import {HttpClient, HttpParams} from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import {Observable} from "rxjs";
 import {CategoryResponse} from "../../core/models/category-model";
 import { CategoryType } from "../../core/types/category-type";
