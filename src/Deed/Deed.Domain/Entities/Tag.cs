6c71bdc8f37e23d80f5007bc493c979f46102883
Maksym Vasylyshyn <maxim24152771@gmail.com>
2025-11-18 17:28:36 +0200

    draft

---
+++ src/Deed/Deed.Domain/Entities/Tag.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deed.Domain.Entities;
+
+public sealed class Tag : Entity, IAuditableEntity
+{
+    public Tag(int id)
+        : base(id)
+    {
+    }
+
+    public Tag()
+    {
+    }
+
+    public required string Name { get; set; }
+
+
+    public DateTimeOffset CreatedAt { get; init; }
+    public int CreatedBy { get; init; }
+    public DateTimeOffset? UpdatedAt { get; init; }
+    public int? UpdatedBy { get; init; }
+
+    public required int ExpenseId { get; init; }
+    public Expense? Expense { get; init; }
+}
