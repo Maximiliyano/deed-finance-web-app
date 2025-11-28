b14ef7782972e18a8ea08c79293091626e3c4ebb
Maksym Vasylyshyn <maxim24152771@gmail.com>
2025-11-28 15:42:41 +0200

    mock changes

---
+++ tests/Deed.Tests.Common/Constants/TestErrors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Deed.Domain.Results;
+
+namespace Deed.Tests.Common.Constants;
+
+internal static class TestErrors
+{
+    internal static class HttpClient
+    {
+        internal Error 
+    }
+}
---
+++ tests/Deed.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Deed.Tests.Common.Mocks;
+
+namespace Deed.Tests.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task CreateCapital_ShouldReturnId()
+    {
+        // Arrange
+        var client = new TestClient();
+
+        // Act
+        await client.SendRequestAsync(HttpMethod.Post, "api/capitals");
+
+        // Assert
+    }
+}
